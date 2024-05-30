49f08e5037b609771b53641cc1ff3705c827c92b
Evan Sellers <github@sellersew.com>
2024-05-30 12:25:33 -0400

    Updated Book Getter by Language

--- src/book/index.ts
+++ src/book/index.ts
@@ -1,14 +1,14 @@
-import { Book, BookMetadata } from "../../resources/index.js";
+import { Book, BookMetadata, Language, Metadata } from "../../resources/index.js";
 
 
 /**
  * Retrieves book metadata by ID or name.
- * @param {readonly BookMetadata[]} metadata - An array of book metadata.
+ * @param {Language} language - An enumerated language code.
  * @param {number | Book} id - The ID or index of the book to retrieve.
  * @returns {BookMetadata | undefined} The metadata of the book if found, otherwise undefined.
  */
-export function GetBook(metadata:readonly BookMetadata[], id:number|Book):BookMetadata|undefined {
-    // FIXME - Make language instead
+export function GetBook(language:Language, id:number|Book):BookMetadata|undefined {
+    const metadata = Metadata[language];
     if (typeof id === "number") {
         if (id < 0 || id >= metadata.length) {
             return undefined;
@@ -21,15 +21,14 @@ export function GetBook(metadata:readonly BookMetadata[], id:number|Book):BookMe
 
 /**
  * Retrieves book metadata for multiple IDs or names.
- * @param {readonly BookMetadata[]} metadata - An array of book metadata.
+ * @param {Language} language - An enumerated language code.
  * @param {(number | Book)[]} [ids] - An optional array of IDs or indexes of the books to retrieve.
  * @returns {readonly BookMetadata[]} An array containing the metadata of the specified books.
  */
-export function GetAllBooks(metadata:readonly BookMetadata[], ids?:(number|Book)[]):readonly BookMetadata[] {
-    // FIXME - Make language instead
+export function GetAllBooks(language:Language, ids?:(number|Book)[]):readonly BookMetadata[] {
     if (!ids) {
-        return metadata;
+        return Metadata[language];
     }
-    return Object.freeze(ids.map(id => GetBook(metadata, id)).filter(b => b !== undefined) as BookMetadata[]);
+    return Object.freeze(ids.map(id => GetBook(language, id)).filter(b => b !== undefined) as BookMetadata[]);
 }
 
