178ed6d5cbc4186b257c15b7386de6720c51b43f
Evan Sellers <github@sellersew.com>
2024-05-30 15:59:51 -0400

    Added Biome

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from "node:path";
 import { build } from "esbuild";
 import TSConfig from "./tsconfig.json" with { type: "json" };
 
@@ -7,8 +7,8 @@ function createBuildSettings(options) {
 	if (TSConfig.files.length > 1) {
 		throw new Error("Only one entry point is supported");
 	}
-	let entries = [TSConfig.files[0].replace("../", "./")];
-	let output  = path.join(TSConfig.compilerOptions.outDir.replace("../", "./"), "index.js");
+	const entries = [TSConfig.files[0]];
+	const output  = path.join(TSConfig.compilerOptions.outDir, "index.js");
 	return {
 		entryPoints: entries,
 		outfile: output,
